e9e6e71a48abe72fce558db4acb1d37508850013
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-15 16:28:15 +0100

    [add] AppCTA finished

--- src/components/AppCallToAction.vue
+++ src/components/AppCallToAction.vue
@@ -5,14 +5,56 @@
 </script>
 
 <template>
-    
+
   <section>
     <div class="container">
-        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-        <img src="../assets/img/buy-comics-merchandise.png" alt="">
-        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        <div class="image">
+                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <div class="text">
+                            <span>digital comics</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="image">
+                            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <div class="text">
+                            <span>dc merchandise</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="image">
+                            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <div class="text">
+                            <span>subscription</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="image">
+                            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <div class="text">
+                            <span>comic shop locator</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="image">
+                            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        </div>
+                        <div class="text">
+                            <span>dc power visa</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        
     </div>
   </section>
 
@@ -28,10 +70,32 @@
         background-color: $primary-color;
         height: 150px;
         .container{
+            height: 100%;
+            width: 100%;
             @include centerFlex('both');
-            img{
-                width: 80px;
+            li{
+                @include centerFlex();
+                a{
+                    color:white;
+                    text-transform: uppercase;
+                    width: 100%;
+                    padding: 0px 25px; 
+                    @include centerFlex();
+                    img{
+                        width: 55px;
+                        min-width: 40px;
+                        height: 55px;
+                        object-fit:scale-down;
+                    }
+                    .text{
+                        margin-left: 10px;
+                    }
+                    &:hover{
+                        text-decoration: underline;
+                    }
+                }
             }
+            
         }
     }
 

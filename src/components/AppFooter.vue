1dfb7342a6718c6896df8c447f9718a279c0e113
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-16 12:33:18 +0100

    [add] v-for AppFootrer links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,132 @@
 <script>
     export default {
-        name: "AppFooter"
+        name: "AppFooter",
+        data(){
+            return{
+                objetcs:[
+                {
+                    title: 'DC Comics',
+                    links: [
+                        {
+                            href: '#',
+                            text: 'Characters',
+                        },
+                        {
+                            href: '#',
+                            text: 'Comics',
+                        },
+                        {
+                            href: '#',
+                            text: 'Movies'
+                        },
+                        {
+                            href: '#',
+                            text: 'TV',
+                        },
+                        {
+                            href: '#',
+                            text: 'Games',
+                        },
+                        {
+                            href: '#',
+                            text: 'Videos',
+                        },
+                        {
+                            href: '#',
+                            text: 'Newss',
+                        },
+                    ],
+                },
+                {
+                    title: 'Shop',
+                    links: [
+                        {
+                            href: '#',
+                            text: 'Shop DC',
+                        },
+                        {
+                            href: '#',
+                            text: 'Shop DC Collectibles',
+                        }
+                    ],
+                },
+                {
+                    title: 'Dc',
+                    links: [
+                        {
+                            href: '#',
+                            text: 'Terms Of Use',
+                        },
+                        {
+                            href: '#',
+                            text: 'Privacy policy(new)',
+                        },
+                        {
+                            href: '#',
+                            text: 'Ad Choices',
+                        },
+                        {
+                            href: '#',
+                            text: 'Adevertaising',
+                        },
+                        {
+                            href: '#',
+                            text: 'Jobs',
+                        },
+                        {
+                            href: '#',
+                            text: 'Subscriptions',
+                        },
+                        {
+                            href: '#',
+                            text: 'Talent Workshops',
+                        },
+                        {
+                            href: '#',
+                            text: 'CPSC Certificates',
+                        },
+                        {
+                            href: '#',
+                            text: 'Ratings',
+                        },
+                        {
+                            href: '#',
+                            text: 'Shop Help',
+                        },
+                        {
+                            href: '#',
+                            text: 'Contac Us',
+                        }
+                    ],
+                },
+                {
+                    title: 'Sites',
+                    links: [
+                        {
+                            href: '#',
+                            text: 'DC',
+                        },
+                        {
+                            href: '#',
+                            text: 'MAD Magazine',
+                        },
+                        {
+                            href: '#',
+                            text: 'DC Kids',
+                        },
+                        {
+                            href: '#',
+                            text: 'DC Universe',
+                        },
+                        {
+                            href: '#',
+                            text: 'DC Power Visa',
+                        }
+                    ],
+                },
+                ]
+            }
+        }
     }
 </script>
 
@@ -13,59 +139,12 @@
 
             <nav>
                 <ul>
-                    <li>
-                        <h3>DC COMICS</h3>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                      
+
+                    <li v-for="(objetc, index) in objetcs" :key="index">
+                        <h3>{{objetc.title}}</h3>
+                        <a v-for="(link, index) in objetc.links" :key="index" href="#">{{link.text}}</a>
                     </li>
-                    <li>
-                        <h3>DC COMICS</h3>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                    </li>
-                    <li>
-                        <h3>DC COMICS</h3>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                    </li>
-                    <li>
-                        <h3>DC COMICS</h3>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                    </li>
+                    
                 </ul>
                 
                 
@@ -93,17 +172,16 @@
             background-repeat: no-repeat;
             background-size: cover;
             .container{
-                position: relative;
                 background-image:url('../assets/img/dc-logo-bg.png') ;
                 background-repeat: no-repeat;
-
+                background-position:right ;
                 nav{
-                    padding-top: 40px;
-                    padding-bottom: 50px;
+                    padding-top: 37px;
+                    padding-bottom: 0px;
                     height: 370px;
-                    width: 25%;
+                    width: 40%;
                     ul{
-                        height: 280px;
+                        height: 300px;
                         display: flex;
                         flex-grow: 0;
                         flex-direction: column;
@@ -111,18 +189,19 @@
                         li{
                             display: flex;
                             flex-direction: column;
-                            margin-bottom: 18px;
-
+                            margin-right: 10px;
                         }
                     }
                     h3{
                         color:white;
                         text-transform: uppercase;
-                        margin-bottom: 18px;
+                        margin: 13px 0; 
+                        font-size: 1.2rem;
                     }
                     a{
                         font-size: .8rem;
-                        color: rgb(182, 182, 182);
+                        margin: 3px 0px;
+                        color: rgb(151, 151, 151);
                         &:hover{
                             text-decoration: underline;
                         }

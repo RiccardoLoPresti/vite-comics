4b9b30ff11f97e31c3586f1972d07ef333e65df5
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-16 15:54:58 +0100

    [add] button load more

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,9 +20,15 @@ import cards from '../data/dc-comics'
 <template>
     <main>
         <div class="container">
-            <AppCards v-for="(card, index) in cards" :key="index" 
-            :cardImg="card.thumb" 
-            :cardTitle="card.series"/>
+            <div class="card-section">
+                <AppCards v-for="(card, index) in cards" :key="index" 
+                :cardImg="card.thumb" 
+                :cardTitle="card.series"/>
+            </div>
+            
+            <div class="button">
+                <button>Load More</button>
+            </div>
         </div>
     </main>
     
@@ -34,7 +40,7 @@ import cards from '../data/dc-comics'
 @use '../src/styles/partials/variables' as *;
 @use '../src/styles/partials/mixin' as *;
 
-    .container{
+    .card-section{
         display: flex;
         justify-content: center;
         height: 100%;
@@ -46,5 +52,24 @@ import cards from '../data/dc-comics'
         padding-top: 50px;
         padding-bottom: 80px;
         color: white;
+    }
+    .button{
+        text-align: center;
+        margin-top: 80px;
+        button{
+            border: none;
+            background-color:$primary-color;
+            width: 180px;
+            height: 35px;
+            color: white;
+            text-transform: uppercase;
+            font-weight: 600;
+            font-size: .8rem;
+            cursor: pointer;
+            &:hover{
+                background-color: lighten($primary-color, 20%);
+            }
+        }
+        
     }
 </style>
